48b826d7e680ec8802132516ac8f24d4881371a0
Alessandro Palazzolo <alessandrorochepalazzolo@gmail.com>
2022-10-12 19:15:35 +0200

    structure + header

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import Header from './sections/header/Header';
+import Body from './sections/body/Body'
+import './App.scss';
+
+function App() {
+  return (
+    <div className="app__wrapper center">
+      <Header />
+      <Body />
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/sections/body/Body.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import './Body.scss'
+
+const Body = () => {
+    return(
+        <div className='body__wrapper'></div>
+    );
+}
+
+export default Body;
