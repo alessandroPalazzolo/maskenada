a4b543d66ef4951535e6910422ccf423cba3212f
Alessandro Palazzolo <alessandrorochepalazzolo@gmail.com>
2022-10-18 12:28:03 +0200

    assets

---
+++ client/src/shared/Backdrop.js
@@ -0,0 +1,13 @@
+const React = require("react");
+
+import './Backdrop.scss';
+
+const Backdrop = () => {
+    const content = <div></div>;
+
+    return (
+        content
+    );
+}
+
+export default Backdrop;
